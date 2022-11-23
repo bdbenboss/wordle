ae9eebbc522d2590b78d2a9584c2d922a01f2b3b
Benjamin Deconinck <bdbenboss@hotmail.com>
2022-11-23 10:54:10 +0100

    update guesses

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 // Je selectionne mon tile container et je lui donne comme variable tileDisplay
-const tileDisplay = document.querySelector('.tile-container');
+const tileDisplay = document.querySelector('.tile-container')
 //console.log(tileDisplay)
 // Je selectionne mon key container et je lui donne comme variable keyboard
-const keyboard = document.querySelector('.key-container');
+const keyboard = document.querySelector('.key-container')
 //console.log(keyboard)
 
 // 3 j'ajoute un mot à Wordle à deviner
@@ -79,31 +79,41 @@ guessRows.forEach((guessRow, guessRowIndex) => {
 })
 //console.log(guessRows)
 
-
 // 1 LE BUT EST DE CREER UN CLAVIER POUR L UTILISATEUR
-
-
 
 // 1-1 Je fais une itération sur l'array keys pour selectionner chaque key
 keys.forEach(key => {
   // 1-2 Je create un tag button en js et le met dans la variable buttonElement
-  const buttonElement = document.createElement('button');
+  const buttonElement = document.createElement('button')
   // console.log(buttonElement)
   // 1-3 On affecte une key à chaque bouton
-  buttonElement.textContent = key;
+  buttonElement.textContent = key
   // console.log(buttonElement)
   // 1-4 On attribut un id pour chaque bouton en l'appelant par le nom de la key
-  buttonElement.setAttribute('id', key);
+  buttonElement.setAttribute('id', key)
   // console.log(buttonElement)
   // 1-5 Je met une balise d'écoute pour mettre en route les clicks
-  buttonElement.addEventListener('click', () => handleClick(key));
+  buttonElement.addEventListener('click', () => handleClick(key))
   // console.log(buttonElement)
   // 1-6 J'affiche le clavier
-  keyboard.append(buttonElement);
-});
+  keyboard.append(buttonElement)
+})
 
 // 1-7 Methode pour afficher les clicks sur la console
-// 3.1 j'associe les keys à handleClick
-const handleClick = (key) => {
-  console.log('clicked', key)
-};
+// 3.1 j'associe les letters à handleClick
+const handleClick = (letter) => {
+  console.log('clicked', letter)
+  addLetter(letter)
+}
+
+// 3.2 je creer la methode addLetter pour ajouter une lettre à la tuile par default
+const addLetter = (letter) => {
+  const tile = document.getElementById('guessRow-' + currentRow + '-tile-' + currentTile)
+  tile.textContent = letter
+  //3.2.1 Je change de tuile à chaque clique sur une lettre
+  guessRows[currentRow][currentTile] = letter
+  // 3.3 j'attribue à la tile un attribut pour pouvoir changer de couleur dans le futur
+  tile.setAttribute('data', letter)
+  currentTile++
+  console.log('guessRows',guessRows)
+}
